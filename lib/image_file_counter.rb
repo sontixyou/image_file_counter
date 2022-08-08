5f599cd3223cb95631902be0fd91cdc700f4f5ba
kenken <angelbeatsviolin@gmail.com>
2022-08-08 22:54:02 +0900

    command test

--- lib/image_file_counter.rb
+++ lib/image_file_counter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "image_file_counter/version"
+require_relative "image_file_counter_utils/cli"
 
 module ImageFileCounter
   class Error < StandardError; end
