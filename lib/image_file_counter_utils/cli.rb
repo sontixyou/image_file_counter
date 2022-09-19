7ef192527fb198b90bff26dfacb8ed791ca7a240
kenken <angelbeatsviolin@gmail.com>
2022-09-19 16:22:27 +0900

    不要なメソッドを削除

--- lib/image_file_counter_utils/cli.rb
+++ lib/image_file_counter_utils/cli.rb
@@ -6,23 +6,6 @@ require 'open3'
 
 module ImageFileCounterUtils
   class CLI < Thor
-    desc 'camelize {snake_case_string}', 'convert {snake_case_string} to {camelCaseString}'
-    def convert_camelize_string(str)
-      puts str.split('_').map { |w|
-             w[0] = w[0].upcase
-             w
-           }.join
-    end
-
-    desc 'snake {CamelCaseString}', 'convert {CamelCaseString} to {snake_case_string}'
-    def convert_snake_case_string(str)
-      puts str
-        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
-        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
-        .tr('-', '_')
-        .downcase
-    end
-
     desc 'get_files_name', 'get files'
     def get_files_name
       file_name_array = ImageFileCounter::Core.generate_files_name
