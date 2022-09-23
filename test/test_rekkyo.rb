25de63c0f7062c50e474e88d035e03bac5843e16
kenken <angelbeatsviolin@gmail.com>
2022-09-23 16:34:29 +0900

    テストファイルをリネーム

--- test/test_rekkyo.rb
+++ test/test_rekkyo.rb
@@ -2,9 +2,9 @@
 
 require 'test_helper'
 
-class TestImageFileCounter < Minitest::Test
+class TestRekkyo < Minitest::Test
   # def test_that_it_has_a_version_number
-  #   refute_nil ::ImageFileCounter::VERSION
+  #   refute_nil ::Rekkyo::VERSION
   # end
 
   # def test_it_does_something_useful
