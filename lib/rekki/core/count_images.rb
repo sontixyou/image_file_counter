f5639a7e22cee1fff55af17bf3e35d9fba389688
kenken <angelbeatsviolin@gmail.com>
2022-11-23 13:51:30 +0900

    select_view_files_path : Fixed for better readability

--- lib/rekki/core/count_images.rb
+++ lib/rekki/core/count_images.rb
@@ -2,7 +2,9 @@
 
 module Rekki
   class Core
+    VIEW_FILE_TYPES = /.html.haml|.slim|.erb|.htm/.freeze
     IMAGE_FILE_TYPES = /.webp|.jpeg|.jpg|.png|.svg/.freeze
+
     def self.generate_files_name
       files_path = []
       Dir.glob('./**/*') do |path|
@@ -26,39 +28,33 @@ module Rekki
       select_image_files
     end
 
-    # rubocop:disable Metrics/CyclomaticComplexity
-    # rubocop:disable Metrics/PerceivedComplexity
+    # rubocop:enable Metrics/CyclomaticComplexity
     def self.select_view_files_path(files_path)
       return 'View files and Ruby files count is 0' if files_path.nil?
 
       view_files_path = files_path.select do |file_path|
-        file_path.include?('.html.haml') || file_path.include?('.slim') || file_path.include?('.erb') || file_path.include?('.html')
+        !file_path.include?('node_modules') && file_path.match?(VIEW_FILE_TYPES)
       end
-      return 'View files and Ruby files count is 0' if view_files_path.count.zero? || view_files_path.nil?
+
+      puts 'View files and Ruby files count is 0' and return if view_files_path.count.zero? || view_files_path.nil?
 
       view_files_path
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
-    # rubocop:enable Metrics/PerceivedComplexity
 
-    # rubocop:disable Metrics/CyclomaticComplexity
+    # select_view_files_and_ruby_filesからの返り値を使用して、それぞれのファイルでgrepを行う。
+    # select_image_filesの画像がヒットするかを判別する。
+    # ヒットした画像のパスが何回登場したのかを配列に持たせる。
+    # できれば、それをグラフ化したい。簡易的ならこんな感じ
+    # kojo: ********
+    # aoki: ****
     # rubocop:disable Metrics/MethodLength
     # rubocop:disable Metrics/AbcSize
     def self.count_images_in_view_file
-      # select_view_files_and_ruby_filesからの返り値を使用して、それぞれのファイルでgrepを行う。
-      # select_image_filesの画像がヒットするかを判別する。
-      # ヒットした画像のパスが何回登場したのかを配列に持たせる。
-      # できれば、それをグラフ化したい。簡易的ならこんな感じ
-      # kojo: ********
-      # aoki: ****
       start_time = Time.now
       puts "start time: #{start_time}"
       files_name = generate_files_name
       view_files_path = select_view_files_path(files_name)
-      puts views_files_path and return if view_files_path.class != Array
-
       image_files_name = select_image_files(files_name)
-      puts image_files_name and return if image_files_name.class != Array
 
       result = {}
 
@@ -70,7 +66,7 @@ module Rekki
           stdout, stderr, status = Open3.capture3(command)
           image_count += stdout.split("\n").count
         end
-        result["#{image}"] = image_count
+        result[image.to_s] = image_count
       end
 
       puts 'IMAGE COUNT RESULT'
@@ -79,7 +75,6 @@ module Rekki
       end
       puts "end time: #{Time.now - start_time}"
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
     # rubocop:enable Metrics/MethodLength
     # rubocop:enable Metrics/AbcSize
 
