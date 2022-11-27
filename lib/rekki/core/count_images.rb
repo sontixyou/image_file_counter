f8ac12ef2888a0bc39e86f3ba25b169edcd6929b
kenken <angelbeatsviolin@gmail.com>
2022-11-27 15:07:52 +0900

    Add-new-image_counter (#14)

--- lib/rekki/core/count_images.rb
+++ lib/rekki/core/count_images.rb
@@ -2,6 +2,9 @@
 
 module Rekki
   class Core
+    VIEW_FILE_TYPES = /.html.haml|.slim|.erb|.htm/.freeze
+    IMAGE_FILE_TYPES = /.webp|.jpeg|.jpg|.png|.svg/.freeze
+
     def self.generate_files_name
       files_path = []
       Dir.glob('./**/*') do |path|
@@ -13,55 +16,43 @@ module Rekki
     end
 
     # rubocop:disable Metrics/CyclomaticComplexity
-    # rubocop:disable Metrics/PerceivedComplexity
     def self.select_image_files(files_path)
       return 'Image files count is 0' if files_path.nil?
 
       select_image_files = files_path.select do |file|
-        if file.include?('.webp') || file.include?('.jpeg') || file.include?('.jpg') || file.include?('.png') || file.include?('.svg')
-          file.gsub!(%r{.[/0-9a-zA-Z_-]+/}, '')
-        end
+        file.gsub!(%r{.[/0-9a-zA-Z_-]+/}, '') if file.include?('images') && file.match?(IMAGE_FILE_TYPES)
       end
-      return 'Image files count is 0' if select_image_files.count.zero? || select_image_files.nil?
+
+      puts 'Image files count is 0' and return if select_image_files.count.zero? || select_image_files.nil?
 
       select_image_files
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
-    # rubocop:enable Metrics/PerceivedComplexity
 
-    # rubocop:disable Metrics/CyclomaticComplexity
-    # rubocop:disable Metrics/PerceivedComplexity
+    # rubocop:enable Metrics/CyclomaticComplexity
     def self.select_view_files_path(files_path)
       return 'View files and Ruby files count is 0' if files_path.nil?
 
       view_files_path = files_path.select do |file_path|
-        file_path.include?('.html.haml') || file_path.include?('.slim') || file_path.include?('.erb') || file_path.include?('.html')
+        !file_path.include?('node_modules') && file_path.match?(VIEW_FILE_TYPES)
       end
-      return 'View files and Ruby files count is 0' if view_files_path.count.zero? || view_files_path.nil?
+
+      puts 'View files and Ruby files count is 0' and return if view_files_path.count.zero? || view_files_path.nil?
 
       view_files_path
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
-    # rubocop:enable Metrics/PerceivedComplexity
 
-    # rubocop:disable Metrics/CyclomaticComplexity
+    # select_view_files_and_ruby_filesからの返り値を使用して、それぞれのファイルでgrepを行う。
+    # select_image_filesの画像がヒットするかを判別する。
+    # ヒットした画像のパスが何回登場したのかを配列に持たせる。
+    # できれば、それをグラフ化したい。簡易的ならこんな感じ
+    # kojo: ********
+    # aoki: ****
     # rubocop:disable Metrics/MethodLength
     # rubocop:disable Metrics/AbcSize
     def self.count_images_in_view_file
-      # select_view_files_and_ruby_filesからの返り値を使用して、それぞれのファイルでgrepを行う。
-      # select_image_filesの画像がヒットするかを判別する。
-      # ヒットした画像のパスが何回登場したのかを配列に持たせる。
-      # できれば、それをグラフ化したい。簡易的ならこんな感じ
-      # kojo: ********
-      # aoki: ****
-      start_time = Time.now
-      puts "start time: #{start_time}"
       files_name = generate_files_name
       view_files_path = select_view_files_path(files_name)
-      puts views_files_path and return if view_files_path.class != Array
-
       image_files_name = select_image_files(files_name)
-      puts image_files_name and return if image_files_name.class != Array
 
       result = {}
 
@@ -73,17 +64,55 @@ module Rekki
           stdout, stderr, status = Open3.capture3(command)
           image_count += stdout.split("\n").count
         end
-        result["#{image}"] = image_count
+        result[image.to_s] = image_count
       end
 
       puts 'IMAGE COUNT RESULT'
       result.each do |k, v|
         puts "#{k}: #{'*' * v}"
       end
-      puts "end time: #{Time.now - start_time}"
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
     # rubocop:enable Metrics/MethodLength
     # rubocop:enable Metrics/AbcSize
+
+    # count_images_in_view_file is too slow. image_counter_in_all_views is Created with the goal of speeding up processing time.
+    def self.image_counter_in_all_views
+      require 'fileutils'
+
+      files_name = generate_files_name
+      view_files_path = select_view_files_path(files_name)
+      image_files_path = select_image_files(files_name)
+      image_files_regex = Regexp.union(image_files_path)
+
+      result = search_image_files(view_files_path, image_files_regex)
+      unsued_image_files = image_files_path - result
+      report_result(unsued_image_files)
+    end
+
+    private
+
+    def search_image_files(view_files_path, image_files_regex)
+      result = []
+      view_files_path.each do |file_path|
+        File.open(file_path, 'r').each_line do |read_line|
+          hit_string = read_line.match(image_files_regex).to_s
+          result << hit_string unless hit_string.empty? || result.include?(hit_string)
+        end
+      end
+      result
+    end
+
+    def report_result(unsued_image_files)
+      puts 'UNUSED IMAGE IS NOTHING' and return if unsued_image_files.empty?
+
+      result_head_message = <<-SUMMARY
+        UNUSED IMAGES LIST
+        ===================
+      SUMMARY
+
+      puts result_head_message
+      unsued_image_files.each { |image| puts image }
+      puts '==================='
+    end
   end
 end
--- lib/rekki_utils/cli.rb
+++ lib/rekki_utils/cli.rb
@@ -7,18 +7,18 @@ require 'open3'
 module RekkiUtils
   class CLI < Thor
     desc 'get_files_name', 'Get files'
-    def get_files_name
-      file_name_array = Rekki::Core.generate_files_name
+    def generate_files_name
+      Rekki::Core.generate_files_name
     end
 
     desc 'get_images_files', 'Get image files'
-    def get_image_files
+    def generate_image_files
       files_name = Rekki::Core.generate_files_name
       Rekki::Core.select_image_files(files_name)
     end
 
     desc 'view_files_and_ruby_files', 'Get view files and ruby files'
-    def get_view_files_path
+    def generate_view_files_path
       files_name = Rekki::Core.generate_files_name
       Rekki::Core.select_view_files_path(files_name)
     end
@@ -26,6 +26,11 @@ module RekkiUtils
     desc 'count_image_file_in_files', 'Count image file in files'
     def count_image_file_in_files
       Rekki::Core.count_images_in_view_file
+    end
+
+    desc 'count_image_file_in_files_FIX-VERSION', 'Count image file in files_FIX-VERSION'
+    def image_counter_in_all_views
+      Rekki::Core.image_counter_in_all_views
     end
   end
 end
